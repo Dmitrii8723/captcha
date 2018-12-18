bc06a5ab3a2d81cf12a6735971904786d699ed61
Dmitrii8723 <dimakharapaev@yandex.ru>
2018-12-18 20:26:15 +1100

    Create default.css and index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 (function() {
   console.log("index.js");
 })();
+
+function onSubmit(token) {
+  alert("thanks " + document.getElementById("firstname").value);
+}
+function validate(event) {
+  event.preventDefault();
+  if (
+    !document.getElementById("firstname").value ||
+    !document.getElementById("lastname").value ||
+    !document.getElementById("email").value
+  ) {
+    alert("You must add text to the required fields");
+  } else {
+    grecaptcha.execute();
+  }
+}
+function onload() {
+  var element = document.getElementById("submit");
+  element.onclick = validate;
+}
